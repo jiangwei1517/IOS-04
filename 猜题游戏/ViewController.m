6d97b0384f69c4242ede5393b748497d65ebdc3e
jiangwei18 <13161194509@163.com>
2017-06-09 14:30:30 +0800

    猜题游戏练习

--- 猜题游戏/ViewController.m
+++ 猜题游戏/ViewController.m
@@ -9,23 +9,33 @@
 #import "ViewController.h"
 #import "QuestionModel.h"
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *iv;
 @property (weak, nonatomic) IBOutlet UILabel *titlej;
 @property (weak, nonatomic) IBOutlet UILabel *mIndex;
 - (IBAction)mNextImg:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *mBtnNext;
 @property(nonatomic, assign)int index;
+@property (weak, nonatomic) IBOutlet UIView *mAnswerView;
+@property (weak, nonatomic) IBOutlet UIView *mOptionView;
 - (IBAction)mBigImage:(UIButton *)sender;
+- (IBAction)ivClick:(UIButton *)sender;
 
 @property(nonatomic, strong)NSArray* questions;
 @property(nonatomic, weak)UIButton* mBackBtn;
 @property(nonatomic, assign)CGRect mFrame;
+@property(nonatomic, assign)BOOL isBigImage;
+
+@property(nonatomic, copy)NSString* curAnswer;
 @end
 
 @implementation ViewController
 
 - (IBAction)mNextImg:(UIButton *)sender {
+    [self goToNextImg];
+}
+
+- (void)goToNextImg {
     self.index++;
     QuestionModel* model = self.questions[self.index];
     [self.iv setImage:[UIImage imageNamed:model.icon] forState:UIControlStateNormal];
@@ -34,9 +44,33 @@
     if (self.index == self.questions.count - 1) {
         self.mBtnNext.enabled = NO;
     }
+    [self.mAnswerView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    [self generateAnswerBtn:model];
+    [self generateOptionView:model];
+    self.curAnswer = model.answer;
+    [self.mOptionView setUserInteractionEnabled:YES];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        NSLog(@"cancel");
+    }
 }
 
 - (IBAction)mBigImage:(UIButton *)sender {
+    [self bigImage];
+}
+
+- (IBAction)ivClick:(UIButton *)sender {
+    if (self.isBigImage) {
+        [self smallImg];
+    } else {
+        [self bigImage];
+    }
+}
+
+- (void)bigImage {
+    self.isBigImage = YES;
     // 1 设置阴影
     UIButton *coverView = [UIButton new];
     self.mBackBtn = coverView;
@@ -47,13 +81,14 @@
     // 2大图提前
     [UIView animateWithDuration:1 animations:^{
         coverView.alpha = 0.5;
-         self.iv.frame = CGRectMake(0, (self.view.frame.size.height-300)/2, 375, 300);
+        self.iv.frame = CGRectMake(0, (self.view.frame.size.height-300)/2, 375, 300);
         [self.view bringSubviewToFront:self.iv];
     }];
     [coverView addTarget:self action:@selector(smallImg) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)smallImg {
+    self.isBigImage = NO;
     if (self.mBackBtn != nil) {
         [UIView animateWithDuration:1 animations:^{
             self.mBackBtn.alpha = 0;
@@ -96,6 +131,105 @@
     [self.titlej setText:model.title];
     self.mIndex.text = [NSString stringWithFormat:@"%d/%lu", self.index+1, self.questions.count];
     self.mFrame = self.iv.frame;
+    [self generateAnswerBtn:model];
+    [self generateOptionView:model];
+    self.curAnswer = model.answer;
+}
+
+- (void)generateAnswerBtn:(QuestionModel*)model {
+    NSUInteger len = model.answer.length;
+    for (int i = 0;i < len;i++) {
+        int margin = 10;
+        int width = 35;
+        int height = 35;
+        UIButton* btnAnswer = [UIButton new];
+        [btnAnswer setBackgroundImage:[UIImage imageNamed:@"btn_answer"] forState:UIControlStateNormal];
+        [btnAnswer setBackgroundImage:[UIImage imageNamed:@"btn_answer_highlighted"] forState:
+         UIControlStateHighlighted];
+        [btnAnswer setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [btnAnswer setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+        int marginLeft = (self.view.frame.size.width - len*width-(len - 1)*margin)/2;
+        int marginTop = (self.mAnswerView.frame.size.height-height)/2;
+        btnAnswer.frame = CGRectMake(marginLeft+i*(width+margin), marginTop, width, height);
+        [btnAnswer addTarget:self action:@selector(resume:) forControlEvents:UIControlEventTouchUpInside];
+        [self.mAnswerView addSubview:btnAnswer];
+    }
+}
+
+- (void)resume:(UIButton*)sender {
+    if (sender.currentTitle != nil) {
+        [sender setTitle:nil forState:UIControlStateNormal];
+        [self.mOptionView setUserInteractionEnabled:YES];
+    }
+    for (UIButton* btn in self.mOptionView.subviews) {
+        if (btn.tag == sender.tag) {
+            btn.hidden = NO;
+        }
+    }
+}
+
+- (void)generateOptionView:(QuestionModel*)model {
+    NSArray* optionArray = model.options;
+    int rowNums = 7;
+    for (int i = 0;i<optionArray.count;i++) {
+        NSString* s = optionArray[i];
+        int margin = 10;
+        int width = 35;
+        int height = 35;
+        UIButton* optionBtn = [UIButton new];
+        [optionBtn setBackgroundImage:[UIImage imageNamed:@"btn_option"] forState:UIControlStateNormal];
+        [optionBtn setBackgroundImage:[UIImage imageNamed:@"btn_option_highlighted"] forState:UIControlStateHighlighted];
+        int marginLeft = (self.view.frame.size.width - rowNums*width-(rowNums - 1)*margin)/2;
+        int marginTop = 10;
+        optionBtn.frame = CGRectMake(marginLeft+(i%7)*(width+margin), marginTop+(i/7)*(height+margin), width, height);
+        [optionBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [optionBtn setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+        optionBtn.titleLabel.font = [UIFont systemFontOfSize:14];
+        [optionBtn setTitle:s forState:UIControlStateNormal];
+        [optionBtn setTitle:s forState:UIControlStateHighlighted];
+        optionBtn.tag = i;
+        [optionBtn addTarget:self action:@selector(clickOptionBtn:) forControlEvents:UIControlEventTouchUpInside];
+        [self.mOptionView addSubview:optionBtn];
+    }
+}
+
+- (void)clickOptionBtn:(UIButton*)sender {
+    BOOL isFull = NO;
+    for (UIButton* btn in self.mAnswerView.subviews) {
+        if (btn.currentTitle == nil) {
+            [btn setTitle:sender.currentTitle forState:UIControlStateNormal];
+            btn.tag = sender.tag;
+            isFull = NO;
+            break;
+        } else {
+            isFull = YES;
+        }
+    }
+    if (isFull) {
+        sender.hidden = NO;
+        [self.mOptionView setUserInteractionEnabled:NO];
+        NSMutableString* answer = [NSMutableString new];
+        for (UIButton* btn in self.mAnswerView.subviews) {
+            [answer appendString:btn.currentTitle];
+        }
+        if ([answer isEqualToString:self.curAnswer]) {
+            for (UIButton* btn in self.mAnswerView.subviews) {
+                [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+            }
+            if (self.index == self.questions.count-1) {
+                UIAlertView* alertView = [[UIAlertView alloc]initWithTitle:@"Success" message:@"成功了" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"再接再厉", nil];
+                [alertView show];
+            }else{
+               [self performSelector:@selector(goToNextImg) withObject:nil afterDelay:1.5];
+            }
+        } else {
+            for (UIButton* btn in self.mAnswerView.subviews) {
+                [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+            }
+        }
+    }else{
+        sender.hidden = YES;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
